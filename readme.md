da261687e58ee5b43e9a114eb9e86ea9c42ca770
quayjunwei <quayjunwei.outlookg.com>
2023-10-09 05:24:43 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Getting started
+
+##### This script will go through each file and folder in the specified directory, replacing spaces with underscores and converting the names to lowercase. It checks if the new name is different from the current name before renaming to avoid unnecessary operations.
+
+1. CD into rename-dir directory
+
+2. Edit rename.sh file
+```bash
+    nano rename.sh
+```
+
+3. Insert file path into "/path/to/your/target/folder"
+```bash
+    target_dir="/path/to/your/target/dir"
+```
+
+4. ```^X then Y``` to save changes and exit nano
+
+5. make script executable with chmod
+```bash
+    chmod +x rename_script.sh
+```
---
+++ rename.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Specify the folder where you want to perform the renaming
+target_dir="/path/to/your/target/dir"
+
+# Check if the folder exists
+if [ -d "$target_dir" ]; then
+  # Iterate over all files and folders in the target folder
+  for item in "$target_dir"/*; do
+    if [ -e "$item" ]; then
+      # Get the current filename or foldername
+      current_name=$(basename "$item")
+      
+      # Convert spaces to underscores and make it lowercase
+      new_name=$(echo "$current_name" | tr ' ' '_' | tr '[:upper:]' '[:lower:]')
+      
+      # Check if the new name is different from the current name
+      if [ "$current_name" != "$new_name" ]; then
+        # Rename the file or folder
+        mv "$item" "$target_dir/$new_name"
+      fi
+    fi
+  done
+else
+  echo "Folder not found: $target_dir"
+fi
